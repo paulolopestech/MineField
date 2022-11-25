31008383802b69bcf87baf8f3ec100b09af2c0f0
Paulo Lopes <paulo.junior@nouvenn.com>
2022-11-25 09:21:06 -0300

    :construction: minefield - in progress

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import params from './src/params';
+import Field from './src/components/Field';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}>
+          Tamanho da grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
